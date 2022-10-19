bcd300ce8d5c7db42106be4f1c5df70145f2ca71
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 09:40:44 +0800

    加入專案檔案。

---
+++ Exec_Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// 計算數字階乘(Factorial)
+			int sum = 1;
+			Console.Write("請輸入乘階:");
+
+			string input = Console.ReadLine();
+			bool canBeInt = int.TryParse(input, out int N);
+			for (int i = 1; i <= N; i++)
+			{
+				sum *= i;
+			}
+			Console.WriteLine($"乘階{N}的值是{sum}");
+
+
+		}
+	}
+}
